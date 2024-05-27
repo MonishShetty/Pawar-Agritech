98f690f76d516408f41cdc865fe455695237f285
Moni <themonishhshetty@gmail.com>
2024-05-27 22:01:14 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,11 @@ import React from 'react'
 import './App.css';
 import Carousel from './Carousel';
 import Content from './Content';
-import Navbar from './Navbar';
 import Service from './Service';
 import Companyinfo from './Companyinfo';
 import Whyus from './Whyus';
 import FAQ from './FAQ';
-import Footer from './Footer';
-import Contract from './Contract';
-import Water from './Water';
+
 import Reviews from './Reviews';
 
 
