634527c4e7b8fb2239013cb94228f12ceac4ccd8
Moni <themonishhshetty@gmail.com>
2024-05-27 23:35:18 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -49,19 +49,19 @@ export default function Navbar() {
           </div>
           <div className="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
             <Link to='/login' className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
-                Login
+              Login
             </Link>
             <Link to='/signup' className="text-white bg-green-900 hover:bg-green-950 focus:ring-4 focus:ring-green-900 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-green-900 dark:hover:bg-green-900 focus:outline-none dark:focus:ring-green-950">
-                Sign up
+              Sign up
             </Link>
-            <button onClick={toggleMobileMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu" aria-expanded={mobileMenuOpen}>
+            <button onClick={toggleMobileMenu} type="button" className="mobile-menu-button inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu" aria-expanded={mobileMenuOpen}>
               <span className="sr-only">Open main menu</span>
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
               </svg>
             </button>
           </div>
-          <div id="mega-menu" className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${mobileMenuOpen ? '' : 'hidden'}`}>
+          <div id="mega-menu" className={`desktop-menu-items items-center justify-between w-full md:flex md:w-auto md:order-1 ${mobileMenuOpen ? 'hidden' : ''}`}>
             <ul className="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
               <li>
                 <Link to="/" className="block py-2 px-3 text-green-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-green-900 md:p-0 dark:text-green-900 md:dark:hover:text-green-900 dark:hover:bg-gray-700 dark:hover:text-green-900 md:dark:hover:bg-transparent dark:border-gray-700">
@@ -111,7 +111,7 @@ export default function Navbar() {
                   <div className="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
                     <ul className="space-y-4" aria-labelledby="mega-menu-dropdown-button-marketplace">
                       <li>
-                        <Link to='farmer' onClick={closeMarketplaceDropdown} className="text-gray-500 dark:text-gray-400 hover:text-green-900 dark:hover:text-green-900">
+                        <Link to='/farmer' onClick={closeMarketplaceDropdown} className="text-gray-500 dark:text-gray-400 hover:text-green-900 dark:hover:text-green-900">
                           Farmers
                         </Link>
                       </li>
